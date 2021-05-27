ccd275946994458d42a39b6b556ece01e98328bf
desmond <desmond.yao@buaa.edu.cn>
2021-05-27 17:42:08 +0800

    fix bug

--- bin/pca.py
+++ bin/pca.py
@@ -9,42 +9,43 @@ from sklearn.decomposition import PCA
 def get_parser():
     parser = argparse.ArgumentParser(description='Principal Component Analysis')
 
-    # 设置训练数据集路径
     parser.add_argument('--train_file', type=str,
                         default='D:/data/csv/pca.csv',
                         help='The file path of the train data file')
 
-    # 设置模型的训练轮数
     parser.add_argument('--n_components', type=int,
                         default=300,
+                        help='The max epoch to train the network')
+
+    parser.add_argument('--save_path', type=str,
+                        default='D:/data/pca',
                         help='The max epoch to train the network')
 
     args = parser.parse_args()
     return args
 
 
-def run(train_files, n_components, save_path=None):
+def run(args):
     start_time = time.time()
-    if not isinstance(train_files, (list, tuple)):
-        train_files = (train_files,)
+
     mesh_list = []
-    if save_path is None:
-        save_path = os.path.dirname(train_files[0])
-    for train_file in train_files:
-        end_time = time.time()
-        print("train_file processing time:%d" % (end_time - start_time))
-        with open(train_file) as f:
-            for line in f:
-                fields = line.strip('\n').split(',')
-                mesh_vertex_file = fields[0]
-                mesh_vertex = np.load(mesh_vertex_file)
-                mesh_list.append(mesh_vertex)
+    if args.save_path is None:
+        save_path = os.path.dirname(args.train_file)
+    else:
+        save_path = args.save_path
+
+    with open(args.train_file) as f:
+        for line in f:
+            fields = line.strip('\n').split(',')
+            mesh_vertex_file = fields[0]
+            mesh_vertex = np.load(mesh_vertex_file)
+            mesh_list.append(mesh_vertex)
 
     data = np.array(mesh_list)
 
     mean = np.mean(data, axis=0)
     np.save(os.path.join(save_path, "mean.npy"), mean)
-    pca = PCA(n_components=n_components)
+    pca = PCA(n_components=args.n_components)
     pca.fit(data)
     end_time = time.time()
     print("Time:%d" % (end_time - start_time))
--- bin/torch2onnx.py
+++ bin/torch2onnx.py
@@ -1,53 +1,44 @@
 import os
+import sys
+import yaml
 import torch
 import argparse
+from easydict import EasyDict as edict
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 
 from model.neural_facial_rigger import NeuralFacialRigger
 
 parser = argparse.ArgumentParser(description='Face rigger model converter')
 parser.add_argument(
-    '--embedding_size',
-    default=100,
-    type=int,
-    help='Embedding size')
-parser.add_argument(
-    '--num_vertices',
-    type=int,
-    default=95820,
-    help='Number of vertices.')
-parser.add_argument(
-    '--pretrained_model',
-    default='./train_hair/model/20210302-120027/120.pth',
+    'pretrained_model',
+    default=None,
     type=str,
     help='Pretrained model weights')
+parser.add_argument(
+    'cfg_file',
+    default=None,
+    type=str,
+    help='Model config in yaml format')
 parser.add_argument(
     '--output_model_name',
     default='test_best_model.onnx',
     type=str,
     help='Output model name')
-# 训练数据做PCA之后的均值文件
-parser.add_argument(
-    '--mean_mesh_file',
-    type=str,
-    default='D:/data/pca/mean.npy',
-    help='the mean file of the training data')
-
-# 训练数据做PCA之后的特征向量
-parser.add_argument(
-    '--feat_vec_file',
-    type=str,
-    default='D:/data/pca/featVec.npy',
-    help='the feature vector of the training data')
 
 
 def convert(args):
-    dummy_input = torch.randn(1, 99, device='cpu')
+
+    with open(args.cfg_file) as f:
+        cfg = edict(yaml.full_load(f))
+
+    dummy_input = torch.randn(1, cfg.TRAIN.NUM_RIG_PARAM, device='cpu')
 
     input_names = ["ctrl"]
     output_names = ["mesh"]
 
-    net = NeuralFacialRigger(out_dim=args.embedding_size, num_vertices=args.num_vertices, mean_mesh_file=args.mean_mesh_file, feat_vec_file=args.feat_vec_file)
-    net.load_state_dict(torch.load(args.pretrained_model))
+    net = NeuralFacialRigger(cfg)
+    net.load_state_dict(torch.load(args.pretrained_model)['state_dict'])
 
     save_path = os.path.dirname(args.pretrained_model)
 
